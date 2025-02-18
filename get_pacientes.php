336a767e01cfbe0d6b1b85be30db9de0960d16c1
Vinícius Borges <viniborg3s@gmail.com>
2025-02-18 00:02:12 -0300

    Arquivos de php para funcionamento do cadastra medico

---
+++ get_pacientes.php
@@ -0,0 +1,18 @@
+<?php
+include 'db.php';
+
+$query = "SELECT id, nome FROM paciente";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    $pacientes = [];
+    while ($row = $result->fetch_assoc()) {
+        $pacientes[] = $row;
+    }
+    echo json_encode(["success" => true, "pacientes" => $pacientes]);
+} else {
+    echo json_encode(["success" => false, "message" => "Nenhum paciente encontrado"]);
+}
+
+$conn->close();
+?>
